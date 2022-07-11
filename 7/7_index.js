1dd27f2782f0d51dec4b7ff35c99b373336ddf39
evertonleonel <everton_leonel@hotmail.com>
2022-07-11 20:39:37 -0300

    exercicio 9

--- 7/7_index.js
+++ 7/7_index.js
@@ -1,15 +1,12 @@
-const data = document.getElementById('data');
-const dia = document.querySelector('.dia');
-const mes = document.querySelector('.mes');
-const ano = document.querySelector('.ano');
+// const data = document.getElementById('data');
+// const dia = document.querySelector('.dia');
+// const mes = document.querySelector('.mes');
+// const ano = document.querySelector('.ano');
 
-const dataSplitada = data.value.split('-')
+// const dataSplitada = data.value.split('-')
 
-dataSplitada.reverse();
+// dataSplitada.reverse();
 
-dia.innerHTML += ' ' + dataSplitada[0];
-mes.innerHTML += ' ' + dataSplitada[1];
-ano.innerHTML += ' ' + dataSplitada[2];
-
-
-
+// dia.innerHTML += ' ' + dataSplitada[0];
+// mes.innerHTML += ' ' + dataSplitada[1];
+// ano.innerHTML += ' ' + dataSplitada[2];
--- 9/9_index.js
+++ 9/9_index.js
@@ -1,5 +1,5 @@
 let botao = document.querySelector('.botao');
-const url = '/8/8_janela.html'
+const url = 'https://www.gx2.com.br/'
 
 function abrirLink(url){
   const link = window.open(url, '_blank');
