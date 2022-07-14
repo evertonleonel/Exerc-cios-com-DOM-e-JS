2ef27b044938704687176abfddd5dfb13c2b7fb6
evertonleonel <everton_leonel@hotmail.com>
2022-07-14 19:57:39 -0300

    45 noot

---
+++ 45/45_index.js
@@ -0,0 +1,9 @@
+const nameInput = document.getElementById('nameInput');
+const button = document.getElementById('')
+
+const godCards = [  {name:'Aegir', power:10}, {name:'Aud', power: 5} ,
+                         {name:'Balder', power:7}, {name:'Bragi', power:4},
+                         {name:'Búri', power:9},   {name: 'Dag', power:10},
+                         {name:'Dellingr', power: 3}];
+
+
