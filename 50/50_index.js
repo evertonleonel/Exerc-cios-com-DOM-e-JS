6a605785dd3f465f6cbb3ed060e07f280f466a80
evertonleonel <everton_leonel@hotmail.com>
2022-07-17 21:09:47 -0300

    desafio 50

---
+++ 50/50_index.js
@@ -0,0 +1,31 @@
+let hours = document.querySelector('.hours');
+let minutes = document.querySelector('.minutes');
+let seconds = document.querySelector('.seconds');
+let ampm = document.querySelector('.ampm');
+
+setInterval ( function (){
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let am = h >= 12 ? 'PM' : 'AM';
+    
+    hours.innerHTML = h + '<span> :</span>';
+    minutes.innerHTML = m + '<span> :</span>';
+    seconds.innerHTML = s + '<span> :</span>';
+    ampm.innerHTML = am;
+    
+    
+    // Converter 24h para 12h;
+    
+    if ( h > 12) {
+        hr = h - 12;
+    }
+    
+    //Adicionar zero se tiver apenas 1 digito;
+    
+    h = ( h < 10 ) ? '0' +  h : h ;
+    m = ( m < 10 ) ? '0' +  m : m ;
+    s = ( s < 10 ) ? '0' +  s : s ;
+
+}, 100)
